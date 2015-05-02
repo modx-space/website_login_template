2d6a77da64d979e58e13ace62c14efcfa557d63b
Shaw <zhengtao.xiao@sap.com>
2015-05-02 18:12:18 +0800

    [Feature] => use Devise as authentication solution

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  # Include devise modules
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :timeoutable, :validatable, :confirmable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501062714) do
+ActiveRecord::Schema.define(version: 20150501065024) do
 
   create_table "account_types", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
@@ -20,16 +20,26 @@ ActiveRecord::Schema.define(version: 20150501062714) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "nick_name",       limit: 255
-    t.string   "real_name",       limit: 255
-    t.string   "email",           limit: 255, null: false
-    t.string   "telephone",       limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "account_type_id", limit: 4
+    t.string   "nick_name",              limit: 255
+    t.string   "real_name",              limit: 255
+    t.string   "email",                  limit: 255,              null: false
+    t.string   "telephone",              limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "account_type_id",        limit: 4
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 255
   end
 
   add_index "users", ["account_type_id"], name: "index_users_on_account_type_id", using: :btree
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "users", "account_types"
 end
